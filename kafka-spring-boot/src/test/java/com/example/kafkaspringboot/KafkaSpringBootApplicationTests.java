27a8dd3bcd957534fd43d8f2e2311436eb3f765c
lyjady <lyj2780866735>
2019-10-07 20:12:21 +0800

    java-api producer

--- kafka-spring-boot/src/test/java/com/example/kafkaspringboot/KafkaSpringBootApplicationTests.java
+++ kafka-spring-boot/src/test/java/com/example/kafkaspringboot/KafkaSpringBootApplicationTests.java
@@ -2,15 +2,23 @@ package com.example.kafkaspringboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KafkaSpringBootApplicationTests {
 
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
     @Test
     public void contextLoads() {
+        for (int i = 0; i < 6; i++) {
+            kafkaTemplate.send("java-api", "springboot-message-" + i);
+        }
     }
 
 }
